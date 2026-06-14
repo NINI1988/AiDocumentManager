c9b391c43dc3cb8c2c34788eba91882abbc80990
Christian Nienhaus <nini16041988-github@yahoo.de>
2026-06-14 21:04:06 +0200

    Remove RentenversicherungHandler and related code; update HANDLERS list

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from handlers.deka import DekaBankHandler
-from handlers.rente import RentenversicherungHandler
 from utils.common import BaseHandler
 
 from .canway import CanwayMeldebescheinigungZurSozialversicherungHandler, CanwayRechnungHandler
@@ -9,8 +8,7 @@ from .canway import CanwayMeldebescheinigungZurSozialversicherungHandler, Canway
 HANDLERS: List[BaseHandler] = [
     CanwayRechnungHandler(),
     CanwayMeldebescheinigungZurSozialversicherungHandler(),
-    RentenversicherungHandler(),
     DekaBankHandler()
 ]
 
-__all__ = ["CanwayRechnungHandler", "CanwayMeldebescheinigungZurSozialversicherungHandler", "RentenversicherungHandler", "DekaBankHandler", "HANDLERS"]
+__all__ = ["CanwayRechnungHandler", "CanwayMeldebescheinigungZurSozialversicherungHandler", "DekaBankHandler", "HANDLERS"]
--- handlers/deka.py
+++ handlers/deka.py
@@ -5,19 +5,6 @@ from utils.common import BaseHandler, HandlerResult
 from utils.matchers import fuzzy_contains, fuzzy_extract_month_year
 
 
-DATE_RE = re.compile(r"datum\s*[:\-–]?\s*\d{1,2}[.]\d{1,2}[.]\d{4}", re.I)
-
-
-def extract_subject_after_date(text: str) -> Optional[str]:
-    lines = [line.strip() for line in text.splitlines()]
-    for i, line in enumerate(lines):
-        if DATE_RE.search(line):
-            for next_line in lines[i + 1 :]:
-                if next_line:
-                    return next_line
-    return None
-
-
 class DekaBankHandler(BaseHandler):
     def get_subfolders(self) -> List[str]:
         return ["Versicherungen\\Investmentfonds Riester - Deka"]
@@ -31,5 +18,6 @@ class DekaBankHandler(BaseHandler):
         elif "Quartalsbericht" in text:
             subj = "Quartalsbericht"
         else:
-            subj = extract_subject_after_date(text) or "Deka Bank"
+            return None
+
         return HandlerResult(subject=subj, date=None, subfolder="Versicherungen\\Investmentfonds Riester - Deka")
